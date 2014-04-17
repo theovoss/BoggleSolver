f2896078218f80639c3598ccc5495c8f3821814a
theovoss <theo.voss973@gmail.com>
2014-04-16 21:57:18 -0400

    Added timing tests for determining how effective the dictionary is.
    dictionary time is less than time.time() resolution while checking
    whether the word is in a readlines() or a read() of the full dictionary
    takes about 0.001 to 0.003 seconds.

--- source/test/integration_tests.py
+++ source/test/integration_tests.py
@@ -1,23 +1,70 @@
 import sys
 import unittest
 import os.path
+import time
 
 sys.path.insert(0,'..\\')
 from load_english_dictionary import e_dict
 
 f_name = "..\\..\\docs\\english_words.txt"
+test_name = "..\\..\\docs\\test_words.txt"
 
-d = e_dict()
-d.read_dictionary(f_name)
+class test_everything(unittest.TestCase):
+    def test_search_speed_vs_raw_read(self):
+        print ("Starting first speed test.")
+        d = e_dict()
+        d.read_dictionary(f_name)
 
-f = open(f_name)
+        t = open(test_name)
+        test_words = t.readlines()
 
-print ("Done reading")
+        f = open(f_name)
+        p = open(f_name)
+        allwords = f.read()
+        alllines = p.readlines()
+        print ("allwords length = " + str(len(allwords)))
+        print ("alllines length = " + str(len(alllines)))
 
-for line in f.readlines():
-    d.is_word(line.lower())
-    assert d.is_word(line.lower())
+        t1 = time.time()
+        t2 = time.time()
+        for a_word in test_words:
+            word = a_word
+            lower = a_word.lower()
+            
+            
+            print ( "Word is: " + word )
+            t1 = time.time()
+            assert d.is_word(lower)
+            t2 = time.time()
+            dict_time = t2-t1
+            print ("Custom data struct time is: " + str(dict_time))
 
-print ("found all words")
+            t1 = time.time()
+            if word not in allwords:
+                assert False
+            t2 = time.time()
+            all_time = t2-t1
+            print ("Word time is: " + str(all_time))
+
+            t1 = time.time()
+            if word not in alllines:
+                assert False
+            t2 = time.time()
+            line_time = t2-t1
+            print ("Line time is: " + str(line_time) + "\n")
+        
+    def test_loads_all_words(self):
+        print ("Testing Load All Words")
+        d = e_dict()
+        d.read_dictionary(f_name)
+
+        f = open(f_name)
+
+        for line in f.readlines():
+            d.is_word(line.lower())
+            assert d.is_word(line.lower())
 
 
+
+if __name__ == '__main__':
+    unittest.main()
