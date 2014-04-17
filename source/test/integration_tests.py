2cf4886cb6a5e68671bce448d5163a9cad985048
theovoss <theo.voss973@gmail.com>
2014-04-16 22:38:39 -0400

    Changed integration tests to assert that e_dict is faster. instead of just printing time diffs
    Occasionally fails against allwords, but most of the time they pass.

--- source/test/integration_tests.py
+++ source/test/integration_tests.py
@@ -11,7 +11,6 @@ test_name = "..\\..\\docs\\test_words.txt"
 
 class test_everything(unittest.TestCase):
     def test_search_speed_vs_raw_read(self):
-        print ("Starting first speed test.")
         d = e_dict()
         d.read_dictionary(f_name)
 
@@ -22,8 +21,6 @@ class test_everything(unittest.TestCase):
         p = open(f_name)
         allwords = f.read()
         alllines = p.readlines()
-        print ("allwords length = " + str(len(allwords)))
-        print ("alllines length = " + str(len(alllines)))
 
         t1 = time.time()
         t2 = time.time()
@@ -31,30 +28,27 @@ class test_everything(unittest.TestCase):
             word = a_word
             lower = a_word.lower()
             
-            
-            print ( "Word is: " + word )
             t1 = time.time()
             assert d.is_word(lower)
             t2 = time.time()
             dict_time = t2-t1
-            print ("Custom data struct time is: " + str(dict_time))
 
             t1 = time.time()
             if word not in allwords:
                 assert False
             t2 = time.time()
             all_time = t2-t1
-            print ("Word time is: " + str(all_time))
 
             t1 = time.time()
             if word not in alllines:
                 assert False
             t2 = time.time()
             line_time = t2-t1
-            print ("Line time is: " + str(line_time) + "\n")
-        
+
+            assert dict_time <= all_time
+            assert dict_time <= line_time
+
     def test_loads_all_words(self):
-        print ("Testing Load All Words")
         d = e_dict()
         d.read_dictionary(f_name)
 
