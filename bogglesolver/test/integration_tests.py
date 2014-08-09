d3dd65c55ee4134e2ffbde8c0b46023faf554c9a
theovoss <theo.voss973@gmail.com>
2014-08-09 10:16:26 -0400

    added speed tests

--- bogglesolver/test/integration_tests.py
+++ bogglesolver/test/integration_tests.py
@@ -10,6 +10,8 @@ import time
 import sqlite3
 import os
 
+import boggleboard
+
 from bogglesolver.load_english_dictionary import Edict
 from bogglesolver.boggle_board import Boggle
 from bogglesolver.solve_boggle import SolveBoggle
@@ -217,6 +219,130 @@ class test_everything(unittest.TestCase):
         os.remove("example.db")
 
         assert num_slower_than_sql <= 1
+
+
+# @unittest.skipUnless(os.getenv(ENV), REASON)
+class test_speed_against_other_libraries(unittest.TestCase):
+
+    """Test my boggle library against other boggle libraries."""
+
+    def test_pypi_init_speeds(self):
+        """Test how fast they load."""
+        other_default_size = 4
+        letters = ['i', 'r', 'e', 'e', 'r', 'i', 'u', 'c', 't', 's', 'i', 'e', 'a', 'n', 'i', 'a']
+
+        t1 = time.time()
+        their_boggle = boggleboard.BoggleBoard(other_default_size, letters)
+        their_trie = boggleboard.Trie(WORD_LIST)
+        t2 = time.time()
+
+        their_time = t2 - t1
+
+        t1 = time.time()
+        my_boggle = SolveBoggle()
+        my_boggle.set_board(other_default_size, other_default_size, letters)
+        t2 = time.time()
+
+        my_time = t2 - t1
+
+        print("My init time is: %s" % my_time)
+        print("Their init time is: %s" % their_time)
+        print("Mine is %s slower." % (my_time / their_time))
+        assert my_time / their_time < 1
+        assert False
+
+    def test_pypi_4_by_4(self):
+        """Test 4x4 against the current boggle board on pypi."""
+        other_default_size = 4
+        letters = ['i', 'r', 'e', 'e',
+                   'r', 'i', 'u', 'c',
+                   't', 's', 'i', 'e',
+                   'a', 'n', 'i', 'a']
+
+        their_boggle = boggleboard.BoggleBoard(other_default_size, letters)
+        their_trie = boggleboard.Trie(WORD_LIST)
+
+        t1 = time.time()
+        their_words = their_boggle.findWords(their_trie)
+        t2 = time.time()
+
+        their_solve_time = t2 - t1
+
+        my_boggle = SolveBoggle()
+        my_boggle.set_board(other_default_size, other_default_size, letters)
+
+        t1 = time.time()
+        my_words = my_boggle.solve()
+        t2 = time.time()
+
+        my_solve_time = t2 - t1
+
+        time_difference = my_solve_time / their_solve_time
+
+        print("I found %s words." % len(my_words))
+        print("They found %s words." % len(their_words))
+        print("Mine is %s percent slower" % time_difference)
+        print("My total time %s\nTheir total time %s" % (my_solve_time, their_solve_time))
+
+        for word in their_words:
+            if word not in my_words:
+                print("I didn't find: %s" % word)
+                assert my_boggle.edict.is_word(word)
+        for word in my_words:
+            assert word in their_words
+        assert len(my_words) == len(their_words)
+        assert time_difference < 1
+        assert False
+
+    def test_pypi_10_by_10(self):
+        """Test 10x10 against the current boggle board on pypi."""
+        other_default_size = 10
+        letters = ['o', 'i', 's', 'r', 'l', 'm', 'i', 'e', 'a', 't',
+                   'g', 'e', 't', 'y', 'r', 'b', 'd', 's', 's', 'h',
+                   'f', 'r', 'h', 'r', 'a', 'e', 'd', 'g', 'l', 'u',
+                   'e', 'i', 'e', 'r', 's', 's', 'o', 'n', 'o', 'a',
+                   'o', 'd', 'e', 'g', 'a', 'o', 'e', 't', 's', 'm',
+                   'e', 'y', 's', 'e', 'e', 'b', 'i', 'd', 't', 'h',
+                   'y', 'm', 'i', 'r', 'p', 'c', 's', 'm', 'r', 'e',
+                   'b', 't', 'o', 'o', 'e', 'i', 'p', 's', 'r', 'u',
+                   's', 'l', 'w', 'o', 'k', 'l', 'c', 't', 's', 'l',
+                   'n', 'l', 'r', 'r', 'e', 'i', 'e', 's', 'g', 't']
+
+        their_boggle = boggleboard.BoggleBoard(other_default_size, letters)
+        their_trie = boggleboard.Trie(WORD_LIST)
+
+        t1 = time.time()
+        their_words = their_boggle.findWords(their_trie)
+        t2 = time.time()
+
+        their_solve_time = t2 - t1
+
+        my_boggle = SolveBoggle()
+        my_boggle.set_board(other_default_size, other_default_size, letters)
+
+        t1 = time.time()
+        my_words = my_boggle.solve()
+        t2 = time.time()
+
+        my_solve_time = t2 - t1
+
+        time_difference = my_solve_time / their_solve_time
+
+        print("I found %s words." % len(my_words))
+        print("They found %s words." % len(their_words))
+
+        print("Mine is %s percent slower" % time_difference)
+        print("My total time %s\nTheir total time %s" % (my_solve_time, their_solve_time))
+        for word in their_words:
+            if word not in my_words:
+                print("I didn't find: %s" % word)
+                assert my_boggle.edict.is_word(word)
+        for word in my_words:
+            assert word in their_words
+        assert len(my_words) == len(their_words)
+        assert time_difference < 1
+        assert False
+
 
 if __name__ == '__main__':
     unittest.main()
